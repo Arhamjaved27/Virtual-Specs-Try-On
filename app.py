0460f740c0913c7a3321c5321e0b60a164a2fc5c
Arhamjaved27 <arhamjaved867@gmail.com>
2025-12-11 17:36:14 +0500

    Set up Flask app

--- app.py
+++ app.py
@@ -1,39 +1,11 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
-
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return redirect(url_for('index'))
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(url_for('index'))
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        dest = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(dest)
-        return redirect(url_for('index'))
-    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
